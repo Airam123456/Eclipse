52ecfcd955f2811f53c611f760b56932a758b457
Airam123456 <perseo_39@hotmail.es>
2021-10-18 14:27:04 +0200

    Empezamos TemaFXML

---
+++ PrimerProyectoFX1/src/module-info.java
@@ -0,0 +1,7 @@
+module PrimerProyectoFX1 {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml;
+	opens controllers to javafx.graphics, javafx.fxml;
+}
